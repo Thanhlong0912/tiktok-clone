3409e494e8c82d8d4f13a08271802ccac0105bb3
Thanhlong0912 <dinhthanhlong47@gmail.com>
2023-10-21 14:08:24 +0700

    AuthOverlay

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import AuthOverlay from "@/app/components/AuthOverlay"
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,7 +13,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <AuthOverlay />
+        {children}
+      </body>
     </html>
   )
 }
