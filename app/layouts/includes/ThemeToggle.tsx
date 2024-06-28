d97f29a6afc92954183cb88114ec23382d70c9cd
Đinh Thành Long <89457665+Thanhlong0912@users.noreply.github.com>
2024-06-28 14:51:02 +0700

    chore

--- app/layouts/includes/ThemeToggle.tsx
+++ app/layouts/includes/ThemeToggle.tsx
@@ -24,14 +24,14 @@ const ThemeToggle = () => {
 
   return (
     <div
-      className='relative w-14 h-8 flex items-center dark:bg-blue-950 bg-yellow-200 cursor-pointer rounded-full p-1'
+      className='relative w-14 h-8 flex items-center dark:bg-cyan-700 bg-yellow-200 cursor-pointer rounded-full p-1'
       onClick={() => setDarkMode(!darkMode)}
     >
       <div
-        className='absolute flex items-center justify-center bg-yellow-200 dark:bg-blue-950 w-6 h-6 rounded-full shadow-md transform transition-transform duration-300'
+        className='absolute flex items-center justify-center bg-yellow-600 dark:bg-blue-500 w-6 h-6 rounded-full shadow-md transform transition-transform duration-300'
         style={darkMode ? { left: '2px' } : { right: '2px' }}
       >
-        {darkMode ? <FaMoon className="text-blue-950" size={20} /> : <BsSunFill className="text-yellow-400" size={20} />}
+        {darkMode ? <FaMoon className="text-white" size={20} /> : <BsSunFill className="text-white" size={20} />}
       </div>
     </div>
   )
