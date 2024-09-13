79d1e5783c8d9ea9d596dc178bfe2fec18b48ac3
Đinh Thành Long <89457665+Thanhlong0912@users.noreply.github.com>
2024-09-13 14:28:24 +0700

    refactor code page

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,47 @@
 "use client"
 
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { usePostStore } from "@/app/stores/post"
 import ClientOnly from "./components/ClientOnly"
 import PostMain from "./components/PostMain"
 import MainLayout from "./layouts/MainLayout"
-import { usePathname } from "next/navigation"
+import { FaSpinner } from "react-icons/fa"
 
 export default function Home() {
+  const { allPosts, setAllPosts } = usePostStore()
+  const [loading, setLoading] = useState(true)
 
-  const pathname = usePathname()
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        await setAllPosts() // Assuming setAllPosts is an async function
+      } catch (error) {
+        console.error("Failed to fetch posts:", error)
+      } finally {
+        setLoading(false)
+      }
+    }
 
-  let { allPosts, setAllPosts } = usePostStore();
-  useEffect(() => { setAllPosts()}, [])
+    fetchPosts()
+  }, [setAllPosts])
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <FaSpinner className="text-rose-400 animate-spin text-3xl" />
+      </div>
+    )
+  }
 
   return (
-    <>
-      <MainLayout>
-        <div className="mt-[45px] w-full max-w-[690px] ml-auto">
-          <ClientOnly>
-            {allPosts.map((post, index) => (
-              <PostMain post={post} key={index} />
-            ))}
-          </ClientOnly>
-        </div>
-      </MainLayout>
-    </>
+    <MainLayout>
+      <div className="mt-[45px] w-full max-w-[690px] ml-auto">
+        <ClientOnly>
+          {allPosts.map(post => (
+            <PostMain post={post} key={post.id} />
+          ))}
+        </ClientOnly>
+      </div>
+    </MainLayout>
   )
 }
